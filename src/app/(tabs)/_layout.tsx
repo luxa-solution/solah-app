6896d34343f1256c02ad30392b96efaadb1c310b
kamaldeen30a <issakamal2443@gmail.com>
2025-12-29 09:09:12 +0100

    changed the adhkar tabBar icon

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
-import { MaterialCommunityIcons, Feather } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
-import { BookOpenText, Bolt } from "lucide-react-native";
+import { BookOpenText, Bolt, ShieldCheck } from "lucide-react-native";
 
 import { useSyncDefaultLocation } from "@/features-settings/hooks";
 import { context } from "@/shared/styles";
@@ -32,9 +32,7 @@ function Layout() {
         name="adhkar"
         options={{
           title: "Adhkar",
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="hands-pray" color={color} size={size} />
-          ),
+          tabBarIcon: ({ color, size }) => <ShieldCheck color={color} size={size} />,
         }}
       />
       <Tabs.Screen
