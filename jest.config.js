dc0b9a3c5f7a02073088ad9c59706169b27a920a
Farooq <ofarooqdev@gmail.com>
2026-01-09 23:02:13 +0300

    fix: update

--- jest.config.js
+++ jest.config.js
@@ -8,4 +8,13 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!(?:.pnpm/)?((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg))',
   ],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/*.test.{js,jsx,ts,tsx}',
+    '!src/**/*.spec.{js,jsx,ts,tsx}',
+    '!src/**/index.{js,jsx,ts,tsx}',
+    '!src/**/types.ts',
+    '!src/**/constants.ts',
+  ],
 };
