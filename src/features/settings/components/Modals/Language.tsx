14a2b1085442c2136d7311b823827d8e27c31d74
AMW <111783177+coderhafiz@users.noreply.github.com>
2026-01-18 07:23:44 +0100

    Refactor Language component to previous code
    Refactor Language component to use a function declaration.

--- src/features/settings/components/Modals/Language.tsx
+++ src/features/settings/components/Modals/Language.tsx
@@ -1,9 +1,3 @@
-import { View, Text } from "react-native";
-
-export const Language = () => {
-  return (
-    <View>
-      <Text>This is a Language</Text>
-    </View>
-  );
-};
+export function Language() {
+  return <></>;
+}
