7a0c115ca1425991575ed49365f07cb73bf48812
Farooq <ofarooqdev@gmail.com>
2026-03-30 01:03:43 +0300

    fix(app): update app config for notifications

--- app.config.ts
+++ app.config.ts
@@ -29,8 +29,8 @@ const SPLASH_ICON = {
   darkBgColor: appIcons.darkBg,
 };
 const NOTIFICATION_AUDIOS = {
-  short: "./assets/notification_sounds/takbir-only.mp3",
-  full: "./assets/notification_sounds/full-adhan.mp3",
+  short: "./assets/notification/takbir-only.mp3",
+  full: "./assets/notification/full-adhan.mp3",
 } as const;
 
 // Environment-specific overrides
